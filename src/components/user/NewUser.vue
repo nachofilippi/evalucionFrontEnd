37c173dba279fd9b22b72e52bfa22638f00cb50e
Ignacio Filippi <ifilippi@zetech.com.ar>
2019-12-03 21:21:44 -0300

    cambio en el crear usuario con valiciones

--- src/components/user/NewUser.vue
+++ src/components/user/NewUser.vue
@@ -37,50 +37,91 @@
   </div>
 </template>
 <script>
-export default {
-    data(){
-        return {
-            link:{ 
-                name:'/user', 
-            },
-            userInformation: [],
-            user:[{
-              id:'',
-              name:'',
-              date:'',
-              gender:'',
-              email:''
-            }],
-          newUser : {
-            id: null,
-            name: '',
-            age: null,
-            gender: '',
-            email: '',
-          }
+import * as moment from 'moment';
 
-        }
-    },
-    created(){
-      this.userInformation = JSON.parse(localStorage.getItem('userInformation'));
-      console.log(this.userInformation);
-    },
-    methods:{
-      save(){
-        this.newUser.id = this.userInformation.length; 
+export default {
+  data() {
+    return {
+      link: {
+        name: '/user',
+      },
+      userInformation: [],
+      user: [{
+        id: '',
+        name: '',
+        date: '',
+        gender: '',
+        email: ''
+      }],
+      newUser: {
+        id: null,
+        name: '',
+        age: null,
+        gender: '',
+        email: '',
+      }
+
+    }
+  },
+  created() {
+    this.userInformation = JSON.parse(localStorage.getItem('userInformation'));
+    // console.log(this.userInformation);
+  },
+  methods: {
+    save() {
+      // difference between today and user's birthday than gives me the age
+      let age = Math.abs(parseInt(moment.duration(moment.utc(this.user.date).diff(moment())).asYears()));
+      // create object to set in localStorage
+      this.newUser.id = this.idUniqueValidation(age);
+      console.log(this.newUser.id);
+      
+      if (this.newUser.id != 0) {
         this.newUser.gender = this.user.gender;
         this.newUser.email = this.user.email;
         this.newUser.name = this.user.name;
-        console.log(this.user.date);
-        
-
-        this.userInformation.push(this.newUser);
+        this.newUser.age = age;
+        if(this.userInformation == null){
+          localStorage.setItem('userInformation', JSON.stringify(this.newUser));
+        }else{
+          this.userInformation.push(this.newUser);
+          localStorage.setItem('userInformation', JSON.stringify(this.userInformation));
+        }
         console.log(this.userInformation);
-        
-        // localStorage.setItem('userInformation', JSON.stringify(this.userInformation));
-        
-      },
-     
+        // set information in object localStorage
+      }
+    },
+    // generate an unique id that not exists in localStorage and validate unique name and mail
+    idUniqueValidation(age) {
+      // random id
+      let id = parseInt(Math.random() * 10000);
+      // validate age 
+      if(age < 18){
+        return 0;
+      }
+      // regex email validation
+      if(this.validateEmail(this.user.email) == false){
+        return 0;
+      }
+      
+      if (id != 0 && this.userInformation != null) {
+        this.userInformation.forEach((user, index) => {
+          // if id exists generates another id
+          if (user.id == id)
+            id = parseInt(Math.random() * 10000);
+          // if mail or name exist, break
+          if ((user.name == this.user.name) || (user.email == this.user.email))
+            return 0;
+        });
+        return id;
+      }
+      // if Object in localStorage is empty
+      if(this.userInformation == null)
+        return id;
+    },
+    validateEmail(email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
     }
+  }
 }
 </script>
